17dc63c21ceb1bd1784347304166bec075f560e4
Breder Couto <104814239+Cout098@users.noreply.github.com>
2024-04-19 11:13:04 -0300

    Create Teste.c

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num = 0;
+
+    do{
+        printf("Escreva um numero\n");
+        scanf("%d", &num);
+    }while (num !=-1);  
+
+}
+
